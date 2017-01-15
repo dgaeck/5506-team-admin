86707473007e257c5aefbaedeae57c808e148f39
dgaeck <dgaeck@myfairpoint.net>
2017-01-15 10:58:03 -0500

    test commit

---
+++ 5506 Team Admin/src/main/test.java
@@ -0,0 +1,13 @@
+package main;
+
+public class test {
+    public static void main(String[] args) {
+        int testscore = 90;
+       
+        if (testscore <= 79){
+        System.out.println("Less than 75");
+        }else {
+        	System.out.println("Greater than 80");
+        }
+    }
+}
